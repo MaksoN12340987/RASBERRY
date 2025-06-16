d20d58ce3099fbecf1780083e8bc9d915b0d5105
Maksim Andreev <andrianov_maksim@outlook.com>
2025-06-16 16:21:56 +0300

    read_byte_data

--- supply/i2c.py
+++ supply/i2c.py
@@ -28,7 +28,7 @@ class SwitchI2C(SMBus):
     
     def __str__(self):
         return f"""Name {self.name_switch}, i2c-{self.bus}:
-    {self.write_quick(0x40)}"""
+    {self.read_byte_data(40, 0)}"""
 
 
 
