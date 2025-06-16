97533df63a2cf501fb1d4fce39732a3e07bd46bc
Maksim Andreev <andrianov_maksim@outlook.com>
2025-06-16 16:55:33 +0300

    testing hex(adress), hex(regster)

--- supply/i2c.py
+++ supply/i2c.py
@@ -20,16 +20,35 @@ import time
 
 class SwitchI2C(SMBus):
     name_switch: str
+    adress_switch: int
+    defolt_registr: int
     
-    def __init__(self, name_switch, bus: int = 1, force: bool = False):
-        self.name_switch = name_switch
-        self.bus = bus
-        super().__init__(bus, force)
-    
-    def __str__(self):
-        with self.SMBus(self.bus):
-            text = f"Name {self.name_switch}, i2c-{self.bus}: \n{self.read_byte_data(0x40, 0x00)}"
+    def __init__(self, name_switch, adress_switch, defolt_registr, bus: int = 1, force: bool = False):
+        validation = self.__validation_input([name_switch, adress_switch, defolt_registr, bus])
         
+        self.bus = validation[bus]
+        self.name_switch = validation[name_switch]
+        self.adress_switch = validation[adress_switch]
+        self.defolt_registr = validation[defolt_registr]
+        super().__init__(bus, force)
+        
+    def __validation_input():
+        
+        return {}
+    
+    def __str__(self, adress: int = 0, regster: int = 0):
+        # if not adress and not regster:
+        #     text = f"Name {self.name_switch}, i2c-{self.bus}: \n{self.read_byte_data(hex(adress), hex(regster))}"
+        # else:
+        #     adress, regster = self.adress_switch, self.defolt_registr
+        #     text = f"Name {self.name_switch}, i2c-{self.bus}: \n{self.read_byte_data(hex(adress), hex(regster))}"
+        # return text
+        
+        if not adress and not regster:
+            text = f"Name {self.name_switch}, i2c-{self.bus}: \n{hex(adress), hex(regster)}"
+        else:
+            adress, regster = self.adress_switch, self.defolt_registr
+            text = f"Name {self.name_switch}, i2c-{self.bus}: \n{hex(adress), hex(regster)}"
         return text
 
 
