b5ede60dfec595f89a52deaf57d1f903c9ef94ee
Maxim Andreev <andrianov_maksim@outlook.com>
2025-07-27 21:08:18 +0300

    redact supply urls

--- supply/urls.py
+++ supply/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path("home/turn_on/<int:pk>/", SwitchON.as_view(), name="turn_on"),
     path("home/turn_off/<int:pk>/", SwitchOFF.as_view(), name="turn_off"),
     path("home/create/", CreateButtonSwitch.as_view(), name="create"),
-    path("home/<int:pk>/update/", ButtonUpdate.as_view(), name="update"),
-    path("home/<int:pk>/delite/", ButtonDelete.as_view(), name="delite"),
+    path("home/update/<int:pk>/", ButtonUpdate.as_view(), name="update"),
+    path("home/delite/<int:pk>/", ButtonDelete.as_view(), name="delite"),
 ]
