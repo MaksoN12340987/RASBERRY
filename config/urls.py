606760114cd87625a8c578ae6b7ad7426c5e2411
Maxim Andreev <andrianov_maksim@outlook.com>
2025-07-29 18:33:45 +0300

    add user config in urls

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,17 @@ from django.contrib import admin
 from django.urls import include, path
 
 from supply.apps import SupplyConfig
+from users.apps import UsersConfig
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
         f"{SupplyConfig.name}/",
         include(f"{SupplyConfig.name}.urls", namespace=f"{SupplyConfig.name}"),
+    ),
+    path(
+        f"{UsersConfig.name}/",
+        include(f"{UsersConfig.name}.urls", namespace=f"{UsersConfig.name}"),
     ),
 ]
 
