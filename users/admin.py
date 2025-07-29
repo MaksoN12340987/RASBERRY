05e26c8062ff90eddd7405e770c1d5c662939237
Maxim Andreev <andrianov_maksim@outlook.com>
2025-07-29 18:20:51 +0300

    add user settings

---
+++ users/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import BaseUser
+
+
+@admin.register(BaseUser)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "is_superuser",
+        "username",
+        "password",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "preview",
+        "email",
+        "phone_number",
+        "id",
+        "location",
+        "date_joined",
+        "last_login",
+    )
+    list_filter = (
+        "username",
+        "is_superuser",
+    )
+    search_fields = ("username",)
