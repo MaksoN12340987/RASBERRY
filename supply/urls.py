f6e40851ab5b440821cefce31b1ee275d492c54a
Maxim Andreev <andrianov_maksim@outlook.com>
2025-09-14 00:35:58 +0300

    add list redacts switches

--- supply/urls.py
+++ supply/urls.py
@@ -9,6 +9,7 @@ from .views import (
     SwitchON,
     SwitchOFF,
     ButtonUpdate,
+    RaedactButtonsView,
 )
 
 app_name = SupplyConfig.name
@@ -17,8 +18,8 @@ urlpatterns = [
     path("home/", SwitchesButtonsView.as_view(), name="home"),
     path("home/turn_on/<int:pk>/", SwitchON.as_view(), name="turn_on"),
     path("home/turn_off/<int:pk>/", SwitchOFF.as_view(), name="turn_off"),
-    path("home/create/", CreateButtonSwitch.as_view(), name="create"),
-    path("home/redact/", ButtonDelete.as_view(), name="redact"),
-    path("home/update/<int:pk>/", ButtonUpdate.as_view(), name="update"),
-    path("home/delite/<int:pk>/", ButtonDelete.as_view(), name="delite"),
+    path("create/", CreateButtonSwitch.as_view(), name="create"),
+    path("redact/", RaedactButtonsView.as_view(), name="redact"),
+    path("update/<int:pk>/", ButtonUpdate.as_view(), name="update"),
+    path("delite/<int:pk>/", ButtonDelete.as_view(), name="delite"),
 ]
